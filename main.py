5d40defff269fb7d2c59c0c1ed698f99ddc03091
yuelianglune <yuelianglune@163.com>
2025-09-09 16:08:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def weight_converter():
         print(f"对应的英制重量为{pd:.3f}磅")
     elif input_str.endswith('pd'):
         pd = float(input_str[:-2])
-        kg = pd / 2.2046 +0.001
+        kg = pd / 2.2046 -0.001
         print(f"对应的公制重量为{kg:.3f}公斤")
     else:
         print("输入格式错误，请以kg或pd结尾")
